d40fad88b8b22d47841d2bf0df893181d336c013
Aaron Costelloe <aaroncosman86@gmail.com>
2024-04-03 19:06:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const apiKey = 'f026e77fa7cd8e90aee1e39c693dd9dc'; // Use your actual OpenWeatherMap API key
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter'){
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+      axios.get(url).then((response) => {
+        setData(response.data);
+      }).catch(error => console.error("Error fetching data:", error));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input 
+          value={location} 
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"/>
+      </div>
+      {/* Conditional rendering based on data availability */}
+      {data.main && (
+        <>
+          <div className="top-">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              <h1>{`${data.main.temp}°C`}</h1>
+            </div>
+            <div className="description">
+              {data.weather && <p>{data.weather[0].main}</p>}
+            </div>
+          </div>
+
+          <div className="bottom">
+            <div className="feels">
+              <p className="bold">{`${data.main.feels_like}°C`}</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className="bold">{`${data.main.humidity}%`}</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              <p className="bold">{`${data.wind.speed} MPH`}</p> <p>Wind Speed</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
